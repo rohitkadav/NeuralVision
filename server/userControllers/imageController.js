b9bd5e1b0e47d1d75b4109ef05bd0a2bfc79f534
Rohit <rohitkadav01@gmail.com>
2025-06-05 18:58:02 +0530

    Image Generation Phase Completed

--- server/userControllers/imageController.js
+++ server/userControllers/imageController.js
@@ -1,8 +1,11 @@
-import userModel from "../models/userModel";
+import axios from "axios";
+import userModel from "../models/userModel.js";
+import FormData from "form-data";
 
 export const generateImage =async (req, res)=> {
     try {
-        const {userId , prompt} = req.body
+        const userId= req.user.id
+        const {prompt} = req.body
         const user = await userModel.findById(userId)
 
         if(!user || !prompt) {
@@ -13,6 +16,22 @@ export const generateImage =async (req, res)=> {
             return res.json ({sucess: false , message : "No Credit Balance", creditBalance: user.creditBalance})
         }
 
+        const formData = new FormData()
+        formData.append('prompt' , prompt)
+
+        const {data } =await axios.post ('https://clipdrop-api.co/text-to-image/v1' , formData ,{
+            headers: {
+          'x-api-key': process.env.CLIPDROP_API,
+            },
+            responseType : 'arraybuffer'
+        })
+
+        const base64Image = Buffer.from(data , 'binary').toString('base64')
+        const resultImage = `data:image/png;base64,${base64Image}`
+
+        await userModel.findByIdAndUpdate (user._id , {creditBalance: user.creditBalance-1})
+
+        res.json({sucess: true , message : "Image Generated", creditBalance : user.creditBalance -1 , resultImage})
         
     }catch (e) {
         console.log(e.message);
