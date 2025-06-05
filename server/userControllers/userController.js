8d8c522e6da0ddef6436b76df87523a590d1fcf8
Rohit <rohitkadav01@gmail.com>
2025-06-05 14:54:37 +0530

    mongodb Setup Completed

---
+++ server/userControllers/userController.js
@@ -0,0 +1,54 @@
+import userModel from "../models/userModel";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { use } from "react";
+
+
+const registerUser = async (req, res) => {
+    try {
+        const {name , email , password} = req.body;
+        
+        if( !name || !email || !password) {
+            return res.json({sucess:false, message : 'Missing Details'})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hasedPassword = await bcrypt.hash(password, salt);
+
+        const userData= {
+            name , email , password : hasedPassword
+        }
+        const newUser = new userModel(userData)
+        const user = await newUser.save()
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+        res.json({sucess : true , token , user : {name: user.name}})
+    }catch(e) {
+        console.log(e);
+        res.json({sucess:false , message : e.message})
+    }
+}
+
+const loginUser = async ()=>{
+    try{
+        const {email, password} = req.body;
+        const user = await userModel.findOne({email})
+
+        if(!user) {
+            return res({sucess:false , message : 'User does not exist'})
+        }
+
+        const isMatch = await bcrypt.compare(password , user.password)
+
+        if(isMatch) {
+            const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+            res.json({sucess : true , token , user : {name: user.name}})
+   
+        }else {
+           return  res({sucess:false , message : 'Invalid credenetials'});
+        }
+    }catch(e){
+        console.log(e);
+        res.json({sucess:false , message : e.message})
+    }
+}
